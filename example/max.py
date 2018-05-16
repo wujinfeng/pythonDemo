924b32b656f478cb73fc5bfedfbcfe57c533bffa
吴金锋 <jinfeng.wu@edaibu.net>
2018-05-16 20:34:02 +0800

    最大值

---
+++ example/max.py
@@ -0,0 +1,12 @@
+# 数字，字符串
+print(max(1, 2))
+print(max('a', 'b'))
+
+# 列表，元祖
+print(max([1, 2]))
+print(max((1, 2)))
+
+print(max((50, 40, 200)))
+print(max((-20, 400, 2000)))
+print(max((-20, -30, -10)))
+print(max((0, 20, -30)))
