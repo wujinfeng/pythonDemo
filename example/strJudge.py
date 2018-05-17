5c88a35b7d57f38c1592a0eebe8df1350f31fd35
吴金锋 <jinfeng.wu@edaibu.net>
2018-05-17 18:26:19 +0800

    字符串判断

---
+++ example/strJudge.py
@@ -0,0 +1,11 @@
+# 字符串的判断
+
+print('测试实例1')
+str = 'runoob.com'
+print(str.isalnum())  # 判断所有字符都是数字或者字母
+print(str.isalpha())  # 判断所有字符都是字母
+print(str.isdigit())  # 判断所有字符都是数字
+print(str.islower())  # 判断所有字符都写小写
+print(str.isupper())  # 判断所有字符都是大写
+print(str.istitle())  # 判断所有单词都是首字母大写，像标题
+print(str.isspace())  # 判断所有字符都是空白字符 \t,\n,\r
